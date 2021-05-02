19b8cbf441b81b04ba377033cc4dbe3b9efafe58
nguyennhuhoa <nhuhoa12k1@gmail.com>
2021-05-02 17:49:16 +0700

    <message>

--- user/models.py
+++ user/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
 class Khach_hang(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(default="", max_length=50)
     sdt = models.CharField(default="", max_length= 15)
     diachi = models.CharField(default="",max_length= 200)
