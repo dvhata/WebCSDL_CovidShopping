e6feba19b57bdd97578b7202ffe09c1aef381f4c
nguyennhuhoa <nhuhoacn@gmail.com>
2021-05-03 10:25:46 +0700

    init

--- user/models.py
+++ user/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
 # Create your models here.
+#default = User.objects.filter(username="")
 
 class Khach_hang(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(default="", max_length=50)
     sdt = models.CharField(default="", max_length= 15)
     diachi = models.CharField(default="",max_length= 200)
