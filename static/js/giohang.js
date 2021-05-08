305114be3867b06e472938c45448a8c85cc8bdf0
ThaoHanh <71579658+dvhata@users.noreply.github.com>
2021-05-09 00:14:34 +0700

    first commit

--- static/js/giohang.js
+++ static/js/giohang.js
@@ -5,7 +5,9 @@ for (var i = 0; i < remove_cart.length; i++) {
   button.addEventListener("click", function () {
     var button_remove = event.target
     button_remove.parentElement.parentElement.remove()
+    updatecart()
   })
+  updatecart()
 }
 
 // update cart 
@@ -15,7 +17,7 @@ function updatecart() {
   var total = 0;
   for (var i = 0; i < cart_rows.length; i++) {
     var cart_row = cart_rows[i]
-    var price_item = cart_row.getElementsByClassName("cart-price ")[0]
+    var price_item = cart_row.getElementsByClassName("cart-price")[0]
     var quantity_item = cart_row.getElementsByClassName("cart-quantity-input")[0]
     var price = parseFloat(price_item.innerText)// chuyển một chuổi string sang number để tính tổng tiền.
     var quantity = quantity_item.value // lấy giá trị trong thẻ input
@@ -23,4 +25,17 @@ function updatecart() {
   }
   document.getElementsByClassName("cart-total-price")[0].innerText = total + 'VNĐ'
   // Thay đổi text = total trong .cart-total-price. Chỉ có một .cart-total-price nên mình sử dụng [0].
+}
+
+// thay đổi số lượng sản phẩm
+var quantity_input = document.getElementsByClassName("cart-quantity-input");
+for (var i = 0; i < quantity_input.length; i++) {
+  var input = quantity_input[i];
+  input.addEventListener("change", function (event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+      input.value = 1;
+    }
+    updatecart()
+  })
 }
