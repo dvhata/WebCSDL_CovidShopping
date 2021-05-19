445736f516ca37ae4379defe9dc13cda4240b6f2
TrongDat <trongdat1505@gmail.com>
2021-05-19 20:32:10 +0700

    Fix staticfiles

--- Wed_Besafe/settings.py
+++ Wed_Besafe/settings.py
@@ -15,8 +15,8 @@ import os
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -89,8 +89,8 @@ WSGI_APPLICATION = 'Wed_Besafe.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 Auth_USER_MODEL = 'user.Khach_hang'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR,'static'),
 ]
 
 MEDIA_URL = '/media/'
