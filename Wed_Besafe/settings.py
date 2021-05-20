022445c1c9d184d2f1bd57d4fce04e7148a805c5
ThaoHanh <71579658+dvhata@users.noreply.github.com>
2021-05-21 00:22:30 +0700

    last commit

--- Wed_Besafe/settings.py
+++ Wed_Besafe/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku 
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +145,5 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL= '/profile/'
 CART_SESSION_ID = 'cart'
 IMPORT_EXPORT_USE_TRANSACTIONS = True
+
+django_heroku.settings(locals())
